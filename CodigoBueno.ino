0c9dd932ec6ab9db54599095c42da2af5a4957db
Francisco Joshua Quintero Montero <143279487+joshuaqm@users.noreply.github.com>
2024-05-22 00:01:44 -0600

    [ADDED] [MAIN]
    Esta es la plantilla a modificar

---
+++ CodigoBueno.ino
@@ -0,0 +1,94 @@
+#include <SoftwareSerial.h>
+#include "DHT.h"
+#define RX 2
+#define TX 3
+#define dht_apin 11 // Analog Pin sensor is connected to
+DHT dhtObject;
+String AP = "TP-Link_4E66";       // AP NAME
+String PASS = "wormhole"; // AP PASSWORD
+String API = "OEOBVWS4YRDPUZH9";   // Write API KEY
+String HOST = "api.thingspeak.com";
+String PORT = "80";
+int countTrueCommand;
+int countTimeCommand; 
+boolean found = false; 
+int valSensor = 1;
+  
+SoftwareSerial esp8266(RX,TX); 
+  
+void setup() {
+  Serial.begin(9600);
+  esp8266.begin(115200);
+  dhtObject.setup(11);
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+}
+
+void loop() {
+  
+ String getData = "GET /update?api_key="+ API +"&field1="+getTemperatureValue()+"&field2="+getHumidityValue();
+ sendCommand("AT+CIPMUX=1",5,"OK");
+ sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+ sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+ esp8266.println(getData);
+ delay(1500);
+ countTrueCommand++;
+ sendCommand("AT+CIPCLOSE=0",5,"OK");
+}
+
+
+String getTemperatureValue(){
+
+   //delay(dhtObject.getMinimumSamplingPeriod());
+   float temperature = dhtObject.getTemperature();
+   Serial.print(" Temperature(C)= ");
+   Serial.println(temperature); 
+   return String(temperature); 
+  
+}
+
+
+String getHumidityValue(){
+
+   //delay(dhtObject.getMinimumSamplingPeriod());
+   float humidity = dhtObject.getHumidity();
+   Serial.print(" Humidity in %= ");
+   Serial.println(humidity);
+   return String(humidity); 
+  
+}
+
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);//at+cipsend
+    if(esp8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+  
+    countTimeCommand++;
+  }
+  
+  if(found == true)
+  {
+    Serial.println("OYI");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  
+  found = false;
+ }
