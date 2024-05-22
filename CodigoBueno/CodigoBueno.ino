1c45927028b74514a892bbfca47bffb95de8cd92
Francisco Joshua Quintero Montero <143279487+joshuaqm@users.noreply.github.com>
2024-05-22 00:13:27 -0600

    [MODIFICATED] [MAIN]
    Codigo modificado para nuestros sensores

--- CodigoBueno/CodigoBueno.ino
+++ CodigoBueno/CodigoBueno.ino
@@ -1,12 +1,12 @@
 #include <SoftwareSerial.h>
-#include "DHT.h"
+//#include "DHT.h"
 #define RX 2
 #define TX 3
-#define dht_apin 11 // Analog Pin sensor is connected to
-DHT dhtObject;
-String AP = "TP-Link_4E66";       // AP NAME
-String PASS = "wormhole"; // AP PASSWORD
-String API = "OEOBVWS4YRDPUZH9";   // Write API KEY
+//#define dht_apin 11 // Analog Pin sensor is connected to
+//DHT dhtObject;
+String AP = "";       // AP NAME
+String PASS = ""; // AP PASSWORD
+String API = "R1BKO7ZAALCOZI45";   // Write API KEY
 String HOST = "api.thingspeak.com";
 String PORT = "80";
 int countTrueCommand;
@@ -15,11 +15,34 @@ boolean found = false;
 int valSensor = 1;
   
 SoftwareSerial esp8266(RX,TX); 
-  
+
+//Variables para el sensor de temperatura
+float suma = 0;
+int sensorTemp = A0;
+float valorTemp = 0;
+
+//Activador del ventilador en base a temperatura
+int motorPin = 9;
+
+//Variables para el sensor de luz
+int sensorLuz = A1;
+int valorLuz = 0;
+
+//Activador del LED en base a la luminosidad
+int ledPin = 10;
+
 void setup() {
+  //Estas son las entradas en el Arduino
+  pinMode(sensorTemp, INPUT);
+  pinMode(sensorLuz, INPUT);
+  
+  //Estas son las salidas del arduino
+  pinMode(motorPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  
   Serial.begin(9600);
   esp8266.begin(115200);
-  dhtObject.setup(11);
+  //dhtObject.setup(11);
   sendCommand("AT",5,"OK");
   sendCommand("AT+CWMODE=1",5,"OK");
   sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
@@ -39,23 +62,41 @@ void loop() {
 
 
 String getTemperatureValue(){
+  for(int i=0; i<5; i++){
+    sensorTemp = analogRead(A0);
+    valorTemp = ((sensorTemp*5000.0)/1023)/10;
+  }
+  suma = valorTemp/2.0;
+  Serial.print("Temperatura en C: ");
+  Serial.println(suma);
 
-   //delay(dhtObject.getMinimumSamplingPeriod());
-   float temperature = dhtObject.getTemperature();
-   Serial.print(" Temperature(C)= ");
-   Serial.println(temperature); 
-   return String(temperature); 
+  if(suma > 30){
+    digitalWrite(motorPin,HIGH);
+  }
+  else{
+  	digitalWrite(motorPin,LOW);
+  }
+  
+   return String(suma); 
   
 }
 
 
 String getHumidityValue(){
-
-   //delay(dhtObject.getMinimumSamplingPeriod());
-   float humidity = dhtObject.getHumidity();
-   Serial.print(" Humidity in %= ");
-   Serial.println(humidity);
-   return String(humidity); 
+  //Sensor de luz (fotoresistor)
+  valorLuz = analogRead(sensorLuz);
+  Serial.println("Valor del fotoresistor:");
+  Serial.println(valorLuz);
+  
+  //Activador del LED que indica que hay demasiada LUZ una vez
+  //que pase el umbral de luminosidad especificado
+  if(valorLuz > 950){
+    digitalWrite(ledPin,HIGH);
+  }
+  else{
+  	digitalWrite(ledPin,LOW);
+  }
+   return String(valorLuz); 
   
 }
 
